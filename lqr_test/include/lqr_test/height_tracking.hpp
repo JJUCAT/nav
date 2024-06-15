0f56db1231f78c642c44041f207d5fc6baf1a73f
Leen <17688010148@163.com>
2024-06-15 22:00:05 +0800

    feat(lqr): test

---
+++ lqr_test/include/lqr_test/height_tracking.hpp
@@ -0,0 +1,22 @@
+#ifndef HEIGHT_TRACKING_HPP_
+#define HEIGHT_TRACKING_HPP_
+
+
+namespace lqr_test {
+
+class Height_tracker
+{
+ public:
+  double GenHeight(const double step=0.5) {
+    
+  }
+ private:
+  
+
+
+}; // Height_tracker
+
+} // namespace lqr_test
+
+
+#endif // HEIGHT_TRACKING_HPP_
